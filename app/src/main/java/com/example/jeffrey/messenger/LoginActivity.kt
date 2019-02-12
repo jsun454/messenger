282cbecf19976722fd360fb0de3b895faa73ecaf
jsun454 <jsun46@umail.ucsb.edu>
2019-02-11 17:35:44 -0800

    Added login activity listeners

--- app/src/main/java/com/example/jeffrey/messenger/LoginActivity.kt
+++ app/src/main/java/com/example/jeffrey/messenger/LoginActivity.kt
@@ -2,10 +2,22 @@ package com.example.jeffrey.messenger
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        loginButton.setOnClickListener {
+            Toast.makeText(this@LoginActivity, "Username: ${usernameRegistrationText.text}\n" +
+                    "Password: ${passwordRegistrationText.text}", Toast.LENGTH_SHORT).show()
+        }
+
+        changeToRegisterActivityButton.setOnClickListener {
+            finish()
+        }
     }
 }
