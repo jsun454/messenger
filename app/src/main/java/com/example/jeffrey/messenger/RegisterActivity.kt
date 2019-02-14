52896789564cae46a7c4aef29fb7a64a749e3930
jsun454 <jsun46@umail.ucsb.edu>
2019-02-14 00:07:16 -0800

    Partially implemented photo selection

--- app/src/main/java/com/example/jeffrey/messenger/RegisterActivity.kt
+++ app/src/main/java/com/example/jeffrey/messenger/RegisterActivity.kt
@@ -1,8 +1,12 @@
 package com.example.jeffrey.messenger
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,7 +18,10 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         selectPhotoButton.setOnClickListener {
-
+            startActivityForResult(
+                Intent(Intent.ACTION_PICK).setType("image/*"),
+                0
+            )
         }
 
         registrationButton.setOnClickListener {
@@ -23,6 +30,21 @@ class RegisterActivity : AppCompatActivity() {
 
         changeToLoginActivityButton.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+
+    var photoUri: Uri? = null
+
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
+            photoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
+            val bitmapDrawable = BitmapDrawable(bitmap)
+            selectPhotoButton.setBackgroundDrawable(bitmapDrawable)
+            selectPhotoButton.text = ""
         }
     }
 
@@ -39,10 +61,15 @@ class RegisterActivity : AppCompatActivity() {
             .addOnSuccessListener {
                 Toast.makeText(this, "Successfully created user with ID: " + it.user.uid,
                     Toast.LENGTH_SHORT).show()
+                storeImage()
             }
             .addOnFailureListener {
                 Toast.makeText(this, "Failed to create user: ${it.message}",
                     Toast.LENGTH_SHORT).show()
             }
+    }
+
+    private fun storeImage() {
+        
     }
 }
