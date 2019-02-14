2f0778a7238f3be827386780db85485bc4c75d73
jsun454 <jsun46@umail.ucsb.edu>
2019-02-14 01:18:14 -0800

    Implemented saving new users to Firebase database

--- app/src/main/java/com/example/jeffrey/messenger/RegisterActivity.kt
+++ app/src/main/java/com/example/jeffrey/messenger/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jeffrey.messenger
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
@@ -7,9 +8,13 @@ import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -35,6 +40,7 @@ class RegisterActivity : AppCompatActivity() {
 
     var photoUri: Uri? = null
 
+    @SuppressLint("SetTextI18n")
     @Suppress("DEPRECATION")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -44,7 +50,7 @@ class RegisterActivity : AppCompatActivity() {
             val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
             val bitmapDrawable = BitmapDrawable(bitmap)
             selectPhotoButton.setBackgroundDrawable(bitmapDrawable)
-            selectPhotoButton.text = ""
+            selectPhotoButton.text = "Change Photo"
         }
     }
 
@@ -59,17 +65,48 @@ class RegisterActivity : AppCompatActivity() {
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
             .addOnSuccessListener {
-                Toast.makeText(this, "Successfully created user with ID: " + it.user.uid,
-                    Toast.LENGTH_SHORT).show()
+                Log.d("Jeffrey", "Successfully created user with ID: ${it.user.uid}")
                 storeImage()
             }
             .addOnFailureListener {
-                Toast.makeText(this, "Failed to create user: ${it.message}",
-                    Toast.LENGTH_SHORT).show()
+                Log.d("Jeffrey", "Failed to create user: ${it.message}")
             }
     }
 
     private fun storeImage() {
-        
+        if(photoUri == null) {
+            return
+        }
+
+        val image = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$image")
+        ref.putFile(photoUri!!)
+            .addOnSuccessListener {
+                Log.d("Jeffrey", "Successfully uploaded image: ${it.metadata?.path}")
+                ref.downloadUrl.addOnSuccessListener { uri ->
+                    Log.d("Jeffrey", "Image file location: $uri")
+                    saveUser(uri.toString())
+                }
+            }
+            .addOnFailureListener {
+                Log.d("Jeffrey", "Failed to upload image: ${it.message}")
+            }
+    }
+
+    private fun saveUser(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val username = usernameRegistrationText.text.toString()
+        val user = User(uid, username, profileImageUrl)
+
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("Jeffrey", "Successfully saved user to database")
+            }
+            .addOnFailureListener {
+                Log.d("Jeffrey", "Failed to save user to database: ${it.message}")
+            }
     }
 }
+
+class User(val uid: String, val username: String, val profileImageUrl: String)
