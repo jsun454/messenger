820488632b5df012208b37832319d1dd6c946d26
jsun454 <jsun46@umail.ucsb.edu>
2019-02-15 22:40:46 -0800

    Reverted app name

--- app/src/main/java/com/example/jeffrey/messenger/NewMessageActivity.kt
+++ app/src/main/java/com/example/jeffrey/messenger/NewMessageActivity.kt
@@ -2,11 +2,14 @@ package com.example.jeffrey.messenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+
+        newMessageRecyclerView
     }
 }
