6d9e05c3d105829e3eb615a60f18e5d57cc82b78
jsun454 <jsun46@umail.ucsb.edu>
2019-02-18 16:49:36 -0800

    Fixed scrolling lag when choosing user to start a new message with

--- app/src/main/java/com/example/jeffrey/messenger/model/UserItem.kt
+++ app/src/main/java/com/example/jeffrey/messenger/model/UserItem.kt
@@ -6,13 +6,14 @@ import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.partial_recipient_row.view.*
 
-class UserItem(val user: User): Item<ViewHolder>() {
+class UserItem(private val user: User): Item<ViewHolder>() {
+
     override fun getLayout(): Int {
         return R.layout.partial_recipient_row
     }
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.itemView.partial_recipient_row_txt_username.text = user.username
-        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.partial_recipient_row_img_user_picture)
+        Picasso.get().load(user.profileImageUrl).fit().into(viewHolder.itemView.partial_recipient_row_img_user_picture)
     }
 }
